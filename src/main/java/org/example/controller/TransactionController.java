b50a83095f83e38be8a1e3320eafc1659fb42bb4
Oleg Rulyov <johstfox@gmail.com>
2023-09-25 19:39:49 +0200

    Commit 25.09.2023 Deleted odd classes

--- src/main/java/org/example/controller/TransactionController.java
+++ src/main/java/org/example/controller/TransactionController.java
@@ -5,6 +5,7 @@ import org.example.dto.TransactionDto;
 import org.example.entity.Transaction;
 import org.example.service.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
@@ -12,7 +13,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("transactions")
+@RequestMapping("/api/transactions")
 public class TransactionController {
 
     @Autowired
@@ -22,17 +23,20 @@ public class TransactionController {
     private Converter<Transaction, TransactionDto> transactionConverter;
 
     @GetMapping
+    @PreAuthorize("hasAuthority('permission:user')")
     public List<TransactionDto> getAll() {
         return transactionService.getAll().stream().
                 map(transactionConverter::toDto).collect(Collectors.toList());
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasAuthority('permission:admin')")
     public TransactionDto getById(@PathVariable("id") Long id) {
         return transactionConverter.toDto(transactionService.getById(id));
     }
 
     @PutMapping("/{accountFrom}/{accountTo}/{amount}")
+    @PreAuthorize("hasAuthority('permission:user')")
     public TransactionDto transfer(@PathVariable("accountFrom") String accountFrom,
                                    @PathVariable("accountTo") String accountTo,
                                    @PathVariable("amount") BigDecimal amount) {
@@ -42,6 +46,7 @@ public class TransactionController {
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasAuthority('permission:admin')")
     public void remove(@PathVariable("id") Long id) {
         transactionService.delete(id);
     }
