df7bc21723e885ffc8a0d7db6cdc34b3ae458068
jostfox <johstfox@gmail.com>
2025-01-05 17:06:17 +0100

    Added logging to file functionality

--- src/main/java/org/example/BankingApp.java
+++ src/main/java/org/example/BankingApp.java
@@ -3,10 +3,24 @@ package org.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.File;
+import java.io.IOException;
+
 @SpringBootApplication
 public class BankingApp {
 
-    public static void main(String[] args) {
+    public static final String LOGFILE = "src/main/resources/operational.log";
+
+    public static void main(String[] args) throws IOException {
+        cleanLogFile();
         SpringApplication.run(BankingApp.class, args);
+    }
+
+    private static void cleanLogFile() throws IOException {
+        File file = new File(LOGFILE);
+        if (file.exists()) {
+            file.delete();
+        }
+        file.createNewFile();
     }
 }
--- src/main/java/org/example/service/AopLogger.java
+++ src/main/java/org/example/service/AopLogger.java
@@ -3,20 +3,38 @@ package org.example.service;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.joda.time.LocalDateTime;
 import org.springframework.stereotype.Component;
 
+import java.io.*;
 import java.util.Arrays;
+
+import static org.example.BankingApp.LOGFILE;
 
 @Component
 @Aspect
 public class AopLogger {
 
+
     @Around(value = "execution(* org.example.controller..*.*(..))")
-    public Object controllerLogger(ProceedingJoinPoint joinPoint) throws Throwable{
-        System.out.println("\nClient calls method " + joinPoint +
-                " with arguments: " + Arrays.toString(joinPoint.getArgs()));
+    public Object controllerLogger(ProceedingJoinPoint joinPoint) throws Throwable {
+        String initialValue = "\nClient calls method " + joinPoint +
+                " with arguments: " + Arrays.toString(joinPoint.getArgs());
+        System.out.println(initialValue);
         Object value = joinPoint.proceed();
         System.out.println("\nMethod replies: " + value);
+        loggingToFile(initialValue + "\nMethod replies: " + value);
         return value;
+    }
+
+    private void loggingToFile(Object value) {
+        try
+                (FileWriter fileWriter = new FileWriter(LOGFILE, true)) {
+            String lineSeparator = "\n----------" + LocalDateTime.now() + "-------------";
+            fileWriter.write(lineSeparator + value + System.lineSeparator());
+        } catch (
+                IOException exception) {
+            System.out.println("Error writing to log file");
+        }
     }
 }
